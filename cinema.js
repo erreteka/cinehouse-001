199361e2492cd10bd1cc0ef299b8592e1be82724
erreteka <erreteka@yahoo.com>
2022-04-20 07:11:51 -0300

    Criando arquivo cinema

--- cinema.js
+++ cinema.js
@@ -1,1 +1,13 @@
-console.log("Criamos o arquivo");
+var catalogo = require("./database/catalogo.json");
+// console.log(catalogo);
+var catalogoString = JSON.stringify(catalogo);
+// console.log(catalogoString);
+var catalogoObj = JSON.parse(catalogoString);
+// console.log(catalogoObj);
+
+function listarTodosOsFilmes(filmes) {
+  for (let i = 0; i < filmes.length; i++) {
+    console.log(filmes[i].titulo);
+  }
+}
+listarTodosOsFilmes(catalogoObj.data);
